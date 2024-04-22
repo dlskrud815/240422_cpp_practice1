f609066605b27d99725d918f5a6f514cec48ea07
dlskrud815 <dlskrud815@gmail.com>
2024-04-22 20:06:12 +0900

    fix condition

--- Exercise2.cpp
+++ Exercise2.cpp
@@ -11,16 +11,16 @@ int main()
 		if (age >= 1 && age <= 7) {
 			std::cout << "유아\n";
 		}
-		else if (age >= 8 && age <= 13) {
+		else if (age <= 13) {
 			std::cout << "초등학생\n";
 		}
-		else if (age >= 14 && age <= 16) {
+		else if (age <= 16) {
 			std::cout << "중학생\n";
 		}
-		else if (age >= 17 && age <= 19) {
+		else if (age <= 19) {
 			std::cout << "고등학생\n";
 		}
-		else if (age >= 20 && age < 200) {
+		else if (age < 200) {
 			std::cout << "성인\n";
 		}
 		else if (age >= 200) {
