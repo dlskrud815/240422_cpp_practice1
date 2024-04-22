383d3bb1581db61f1f9d491b9e1e53e3d8ad0340
dlskrud815 <dlskrud815@gmail.com>
2024-04-22 20:12:39 +0900

    fix condition

--- Exercise2.cpp
+++ Exercise2.cpp
@@ -8,33 +8,35 @@ int main()
 		std::cout << "\n나이를 입력하세요.\n";
 		std::cin >> age;
 
-		if (age >= 1 && age <= 7) 
-		{
-			std::cout << "유아\n";
-		}
-		else if (age <= 13) 
-		{
-			std::cout << "초등학생\n";
-		}
-		else if (age <= 16) 
-		{
-			std::cout << "중학생\n";
-		}
-		else if (age <= 19) 
-		{
-			std::cout << "고등학생\n";
-		}
-		else if (age < 200) 
-		{
-			std::cout << "성인\n";
-		}
-		else if (age >= 200) 
-		{
-			std::cout << "나이가 너무 많습니다..!\n";
-		}
-		if (age == -1) 
+		if (age == -1)
 		{
 			return 0;
+		}
+		else {
+			if (age >= 1 && age <= 7)
+			{
+				std::cout << "유아\n";
+			}
+			else if (age <= 13)
+			{
+				std::cout << "초등학생\n";
+			}
+			else if (age <= 16)
+			{
+				std::cout << "중학생\n";
+			}
+			else if (age <= 19)
+			{
+				std::cout << "고등학생\n";
+			}
+			else if (age < 200)
+			{
+				std::cout << "성인\n";
+			}
+			else if (age >= 200)
+			{
+				std::cout << "나이가 너무 많습니다..!\n";
+			}
 		}
 	}
 }
