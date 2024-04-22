0c491d261204b8ef3d93d3beb36ea7918e95019f
dlskrud815 <dlskrud815@gmail.com>
2024-04-22 17:34:47 +0900

    Add project files.

---
+++ 240422_cpp_practice1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main() {
+
+	std::string test3 = "포스코";
+	std::string test4 = test3;
+
+	test3 = "코딩온";
+
+	std::cout << "내가 할 말은 " << std::endl
+		<< test3 + test4 << "\n" << " 입니다.";
+
+	return 0;
+}
